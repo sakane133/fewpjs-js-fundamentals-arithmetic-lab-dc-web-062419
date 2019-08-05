1872d81230ea27e49c9d9f120dd24b49761eea4e
Samantha Kane <s.kane133@gmail.com>
2019-08-05 16:56:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 19 % 5;
+
+const max = Math.max(1, 2, 3, 20, 7);
